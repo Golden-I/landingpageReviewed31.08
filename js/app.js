06ac4f262b3908d87100f0b946159757270d4131
Golden-I <65423579+Golden-I@users.noreply.github.com>
2022-09-01 23:21:37 +0300

    with links

--- js/app.js
+++ js/app.js
@@ -11,17 +11,17 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
  * (resources: https://www.aleksandrhovhannisyan.com/blog/responsive-navbar-tutorial/)
-*/
+ */
 let navbarList = document.querySelector("#navbar__list"); /*https://www.w3schools.com/jsref/met_document_queryselectorall.asp****/
 const sections = document.querySelectorAll("section"); /*https://www.codegrepper.com/code-examples/javascript/queryselectorall+in+javascript***/
 
@@ -29,45 +29,45 @@ const sections = document.querySelectorAll("section"); /*https://www.codegrepper
 /**
  * End Global Variables
  * Start Helper Functions
-*/
+ */
 //check if an element is in viewport or not (https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport/#:~:text=Use%20the%20getBoundingClientRect()%20method%20to%20get%20the%20size%20of,in%20the%20viewport%20or%20not.)
 function isInViewport(elem) {
-	let distance = elem.getBoundingClientRect();
+    let distance = elem.getBoundingClientRect();
 
-	return (
-		distance.top >= -300 &&
-		distance.left >= 0 &&
-		distance.bottom <= (1.3 * window.innerHeight || 
+    return (
+        distance.top >= -300 &&
+        distance.left >= 0 &&
+        distance.bottom <= (1.3 * window.innerHeight ||
             document.documentElement.clientHeight) &&
-		distance.right <= (window.innerWidth || 
+        distance.right <= (window.innerWidth ||
             document.documentElement.clientWidth)
-	);
+    );
 };
 
-    // remove active classes
-    //https://stackoverflow.com/questions/8379776/add-remove-active-class-from-a-navigation-link
-    //https://www.javatpoint.com/add-and-remove-the-active-class-from-a-navigation-link
-    // https://awik.io/check-if-element-is-inside-viewport-with-javascript/
-    // https://stackoverflow.com/questions/61585027/remove-class-from-all-elements-except-active-element
+// remove active classes
+//https://stackoverflow.com/questions/8379776/add-remove-active-class-from-a-navigation-link
+//https://www.javatpoint.com/add-and-remove-the-active-class-from-a-navigation-link
+// https://awik.io/check-if-element-is-inside-viewport-with-javascript/
+// https://stackoverflow.com/questions/61585027/remove-class-from-all-elements-except-active-element
 
-    function deactivateSections() {
-        sections.forEach((element)=>{
-            element.classList.remove("your-active-class", "active");
-        });
-    }
-    
-    function deactivateNavLinks() {
-        let navbarAnchors = document.querySelectorAll(".nav__hyperlink");
-        navbarAnchors.forEach((element)=>{
-            element.classList.remove("active-nav");
-        });
-    }
+function deactivateSections() {
+    sections.forEach((element) => {
+        element.classList.remove("your-active-class", "active");
+    });
+}
+
+function deactivateNavLinks() {
+    let navbarAnchors = document.querySelectorAll(".nav__hyperlink");
+    navbarAnchors.forEach((element) => {
+        element.classList.remove("active-nav");
+    });
+}
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 // build the nav
 window.addEventListener('load', buildNavbar())
@@ -85,11 +85,11 @@ function activateCurrentSection(currentSection) {
 function activateNavLinks(currentSectionId) {
     let navbarAnchors = document.querySelectorAll(".nav__hyperlink");
     //console.log(navbarAnchors);
-        navbarAnchors.forEach((element)=>{
-            if(element.getAttribute('href') == `#${currentSectionId}`) {
-                element.classList.add("active-nav");
-            }
-        });
+    navbarAnchors.forEach((element) => {
+        if (element.getAttribute('href') == `#${currentSectionId}`) {
+            element.classList.add("active-nav");
+        }
+    });
 }
 
 // Scroll to anchor ID using scrollTO event
@@ -111,17 +111,17 @@ function scrollToSectionOnClick() {
  * End Main Functions
  * Begin Events
  * 
-*/
+ */
 
 // Build menu 
 //https://itnext.io/build-a-single-page-web-app-javascript-and-the-dom-90c99b08f8a9
 
 function buildNavbar() {
-	sections.forEach((element)=>{
-	    let listItem = document.createElement("li");
-	    listItem.classList.add("navbar__list__item");
-    	let sectionName = element.getAttribute("data-nav");
-    	let currentSectionId = element.getAttribute("id");
+    sections.forEach((element) => {
+        let listItem = document.createElement("li");
+        listItem.classList.add("navbar__list__item");
+        let sectionName = element.getAttribute("data-nav");
+        let currentSectionId = element.getAttribute("id");
         listItem.innerHTML = `<a href="#${currentSectionId}" class="nav__hyperlink">${sectionName}</a>`;
         navbarList.appendChild(listItem);
     });
@@ -133,16 +133,16 @@ function buildNavbar() {
 scrollToSectionOnClick();
 
 // Set sections as active
-window.addEventListener('scroll', function (event) {
-	event.preventDefault();
-	
+window.addEventListener('scroll', function(event) {
+    event.preventDefault();
+
     sections.forEach((element) => {
         // console.log(element);
         if (isInViewport(element)) {
             deactivateSections();
             activateCurrentSection(element);
             // console.log('In viewport!');
-        } else if(window.scrollY==0) {
+        } else if (window.scrollY == 0) {
             deactivateSections();
             deactivateNavLinks();
             // console.log('No Change');
@@ -150,16 +150,16 @@ window.addEventListener('scroll', function (event) {
     }, false);
 });
 
-// scrollup
-https://blog.magezon.com/how-to-make-a-good-landing-page-in-html-ecm/#h-making-the-scroll-to-top-button
-const scrollUp = document.querySelector("#scroll-up");
+// scroll-up
+https: //blog.magezon.com/how-to-make-a-good-landing-page-in-html-ecm/#h-making-the-scroll-to-top-button
+    const scrollUp = document.querySelector("#scroll-up");
 
 scrollUp.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
 });
 
 // contactForm 
@@ -167,7 +167,7 @@ scrollUp.addEventListener("click", () => {
 //https://stackoverflow.com/questions/6799533/how-to-submit-a-form-with-javascript-by-clicking-a-link
 //https://blog.magezon.com/how-to-make-a-good-landing-page-in-html-ecm/#h-making-the-scroll-to-top-button
 // this is the link I used for this code: https://developer.mozilla.org/en-US/docs/Learn/Forms/Sending_forms_through_JavaScript
-const form  = document.getElementById('contactForm');
+const form = document.getElementById('contactForm');
 
 form.addEventListener('submit', () => {
     // handle the form data, the call back function 
